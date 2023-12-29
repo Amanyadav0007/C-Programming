dd89933e9a010f0d4b1a3a3e8f42bfe45504ded9
@curious_coder_aman <113257634+Amanyadav0007@users.noreply.github.com>
2023-12-30 01:20:37 +0530

    WAP to create array.

--- Array/1-arrayCreation.c
+++ Array/1-arrayCreation.c
@@ -1,1 +1,26 @@
+// How to create a array by user given values...
+#include<stdio.h>
 
+int main()
+{
+    // taking size of array by user
+    int size, i;
+    printf("Enter the size of array : ");
+    scanf("%d", &size);
+
+    // array declaration & taking inputs
+    int arr[size];
+    printf("Enter %d element of array : ", size);
+    for (i = 0; i < size; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    // printing array values
+    printf("Here is your current array : ");
+    for (i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
